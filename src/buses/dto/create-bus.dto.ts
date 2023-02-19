2c11a2f73ab744a5a03e32d1b933ae345565f2dd
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-02-19 16:17:12 -0300

    feat: Created the validation to bus dto

--- src/buses/dto/create-bus.dto.ts
+++ src/buses/dto/create-bus.dto.ts
@@ -1,6 +1,19 @@
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateBusDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsInt()
+  @IsNotEmpty()
   capacity: number;
+
+  @IsString()
+  @IsNotEmpty()
   year: string;
+
+  @IsString()
+  @IsNotEmpty()
   color: string;
 }
