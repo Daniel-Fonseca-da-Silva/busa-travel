6b8a7b97df064853f9b652030e125d4779d10fc7
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-02-19 16:06:46 -0300

    feat: Created the validation to user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,24 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  isNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsInt()
+  @IsNotEmpty()
   age: number;
+
+  @IsString()
+  @IsNotEmpty()
   document: string;
+
+  @IsNumber()
   amount: number;
 }
