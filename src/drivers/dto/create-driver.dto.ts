e12446c24c24a7a991802fd652dc116eaa6ee62a
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-02-19 16:25:34 -0300

    feat: Created the validation to driver dto

--- src/drivers/dto/create-driver.dto.ts
+++ src/drivers/dto/create-driver.dto.ts
@@ -1,6 +1,18 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateDriverDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   age: number;
+
+  @IsString()
+  @IsNotEmpty()
   driverLicense: string;
+
+  @IsNumber()
   salary: number;
 }
