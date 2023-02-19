3be52f03e1dde3853da57a138c498556615298c8
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-02-19 14:48:35 -0300

    feat: Created the rest to user

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  name: string;
+  age: number;
+  document: string;
+  amount: number;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,11 @@
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  id: number;
+  name: string;
+  age: number;
+  document: string;
+  createdAt: Date;
+  updateAt: Date;
+  amount: number;
+}
